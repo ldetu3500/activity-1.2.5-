197ec1233bbf1e2d512111cc731214272a2c3ca5
ldetu3500 <77115685+ldetu3500@users.noreply.github.com>
2021-01-13 10:45:07 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
-# Activity 1.2.5 
-# mad libs game concept coding 
+#Richa shenoy and Lucia De Tuya 
+#1.2.5 project 
+#MAD LIBS VALENTINES EDITION 
 
-import r as random 
-import trtl as Turtle 
+import random as r
+import turtle as trtl 
+
+# this is the code to draw the background of the MADLIBS 
+
+
+drawer=trtl.Turtle()
+
+def small_rectangle(): 
+    drawer.color("black")
+    s = 0 
+    while s < 4:
+        drawer.forward(50) 
+        drawer. right(90)
+        s = s+1 
+
+def large_rectangle():
+    drawer.color("red")
+    l = 0 
+    while l <4:
+        drawer.forward(100)
+        drawer.right(90)
+        l=l+1 
